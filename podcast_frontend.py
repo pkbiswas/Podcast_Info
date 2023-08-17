c142cffa1d0cda04cdc139bf5f3262c6f009275c
pkbiswas <pkbiswas@acm.org>
2023-08-17 01:57:45 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -38,8 +38,8 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        #col3, col4 = st.columns([3, 7])
-        col3 = st.columns([3])
+        col3, col4 = st.columns([3, 7])
+        
 
         with col3:
             st.subheader("Podcast Guest")
@@ -88,8 +88,7 @@ def main():
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
         # Display the podcast guest and their details in a side-by-side layout
-        #col3, col4 = st.columns([3, 7])
-        col3 = st.columns([3])
+        col3, col4 = st.columns([3, 7])
 
         with col3:
             st.subheader("Podcast Guest")
