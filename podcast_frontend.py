5b8276e7af102e081cedad6d223e24402d7f7c39
pkbiswas <pkbiswas@acm.org>
2023-08-19 03:57:12 -0400

    Update podcast_frontend.py
    Changed to the earlier version

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -43,12 +43,12 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            # st.write(podcast_info['podcast_guest']['name'])
-            st.write(podcast_info['podcast_guest'])
+            st.write(podcast_info['podcast_guest']['name'])
+            #st.write(podcast_info['podcast_guest'])
 
-        # with col4:
-            # st.subheader("Podcast Guest Details")
-            # st.write(podcast_info["podcast_guest"]['summary'])
+         with col4:
+             st.subheader("Podcast Guest Details")
+             st.write(podcast_info["podcast_guest"]['summary'])
 
         # Display the five key moments
         st.subheader("Key Moments")
@@ -92,11 +92,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            # st.write(podcast_info['podcast_guest']['name'])
-            st.write(podcast_info['podcast_guest'])
-        # with col4:
-            # st.subheader("Podcast Guest Details")
-            # st.write(podcast_info["podcast_guest"]['summary'])
+            st.write(podcast_info['podcast_guest']['name'])
+            #st.write(podcast_info['podcast_guest'])
+         with col4:
+             st.subheader("Podcast Guest Details")
+             st.write(podcast_info["podcast_guest"]['summary'])
 
         # Display the five key moments
         st.subheader("Key Moments")
